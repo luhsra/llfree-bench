9141b6b2d1f6e5bd9705f696033c29ee9fe3ed14
Lars Wrenger <wrenger@sra.uni-hannover.de>
2022-10-19 17:54:39 +0200

    :sparkles: New local bare-metal benchmarks

---
+++ rand-local.py
@@ -0,0 +1,42 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import json
+from subprocess import check_output, STDOUT
+import shlex
+
+from utils import timestamp
+
+
+def main():
+    parser = ArgumentParser(
+        description="Running the rand benchmark and measuring the page allocations")
+    parser.add_argument("exe", default="./rand")
+    parser.add_argument("-m", "--mem", default=32, type=int)
+    parser.add_argument("-c", "--cores", nargs="+", type=int, required=True)
+    parser.add_argument("-s", "--sockets", type=int, default=2)
+    parser.add_argument("-i", "--iterations", type=int, default=4)
+    parser.add_argument("--args", default="--private")
+    args = parser.parse_args()
+
+    root = Path("rand") / timestamp()
+    root.mkdir(parents=True, exist_ok=True)
+    with (root / "meta.json").open("w+") as f:
+        json.dump(vars(args), f)
+
+    print("run")
+
+    with (root / "out.csv").open("w+") as f:
+        f.write("x,iteration,mem,map,amin,aavg,amax,fmin,favg,fmax,unmap\n")
+        f.flush()
+
+        for c in args.cores:
+            for i in range(args.iterations):
+                out = check_output([args.exe, f"-t{c}", f"-m{args.mem}", *shlex.split(args.args)],
+                                   text=True, stderr=STDOUT, timeout=600.0)
+                result = out.splitlines(False)[1]
+                f.write(f"{c},{i},{args.mem},{result}\n")
+                f.flush()
+
+
+if __name__ == "__main__":
+    main()
