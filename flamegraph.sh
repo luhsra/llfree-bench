3894026362775b62036a9790afa976595adb95e8
Alexander Halbuer <halbuer@sra.uni-hannover.de>
2023-01-03 13:49:42 +0100

    Execute benchmarks on real hardware

--- flamegraph.sh
+++ flamegraph.sh
@@ -4,7 +4,7 @@ FLAMEGRAPH_PATH=/srv/scratch/wrenger/FlameGraph
 OUT="flamegraph/$(date '+%y%m%d_%H%M')"
 mkdir -p $OUT
 
-perf record -F 99 -g -- $@
+perf record -F 99 -ga -- $@
 perf script > $OUT/out.perf
 
 $FLAMEGRAPH_PATH/stackcollapse-perf.pl --all $OUT/out.perf > $OUT/out.folded
