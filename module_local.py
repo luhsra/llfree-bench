b54415dabd4351e2b682d39e8ceb23695b5aeec6
Lars Wrenger <wrenger@sra.uni-hannover.de>
2023-01-04 11:10:09 +0100

    :art: Minor refactoring

--- module_local.py
+++ module_local.py
@@ -1,10 +1,7 @@
 from argparse import ArgumentParser
 from time import sleep, time
-import shlex
-import os
-from subprocess import Popen, PIPE, STDOUT, check_call, check_output
-from utils import SSHExec, non_block_read, qemu_vm, rm_ansi_escape, setup
-import signal
+from subprocess import Popen, PIPE, STDOUT, check_call
+from utils import non_block_read, rm_ansi_escape, setup
 
 
 def main():
@@ -19,88 +16,61 @@ def main():
     parser.add_argument("--module")
     parser.add_argument("--kernel", required=False)
     parser.add_argument("benches", type=str, nargs="+")
-    args, root = setup("module", parser, custom="vm")
+    args, root = setup("module", parser, custom="local")
 
-    ssh = SSHExec(args.user, port=args.port)
-    dmesgp = Popen(["sudo dmesg --follow"], stdout=PIPE, stderr=STDOUT, text=True, shell=True)
+    dmesgp = Popen(["sudo dmesg --follow"], stdout=PIPE,
+                   stderr=STDOUT, text=True, shell=True)
+
+    with (root / "boot.txt").open("w+") as f:
+        f.write(rm_ansi_escape(non_block_read(dmesgp.stdout)))
 
     for bench in args.benches:
         dir = root / bench
         dir.mkdir(parents=True, exist_ok=True)
 
-
         print(f"bench {bench}")
 
         try:
-            # print("start qemu...")
-            # qemu = qemu_vm(args.kernel, args.mem, max(
-            #     args.cores), args.port)
-
-            # print("started")
-            # with (dir / "cmd.sh").open("w+") as f:
-            #     f.write(shlex.join(qemu.args))
-            # with (dir / "boot.txt").open("w+") as f:
-            #     f.write(rm_ansi_escape(non_block_read(qemu.stdout)))
-
-            # print("load module")
-            # if args.module:
-            #     ssh.upload(args.module)
-            # os.system(f"sudo insmod {args.module}")
-
             for order in args.orders:
-                print("configure")
-                allocs = (((args.mem * (512 ** 2)) // max(args.cores)) // 2) // (1 << order)
+                allocs = (((args.mem * (512 ** 2)) //
+                          max(args.cores)) // 2) // (1 << order)
                 core_list = ','.join([str(c) for c in args.cores])
-                print(f"allocate half the memory ({allocs} on {core_list}, o={order})")
+                print(
+                    f"run ({allocs} on {core_list} o={order} i={args.iterations})")
 
-                # with (dir / "running.txt").open("a+") as f:
-                #     f.write(rm_ansi_escape(non_block_read(qemu.stdout)))
+                benchp = Popen(
+                    [f"echo '{bench} {args.iterations} {allocs} {order} {core_list}' | sudo tee /proc/alloc/run"], shell=True)
 
-                print(f"run order={order}")
-                benchp = Popen([f"echo '{bench} {args.iterations} {allocs} {order} {core_list}' | sudo tee /proc/alloc/run"], shell=True)
-
-                timeout = time() + 600.0 # seconds
+                timeout = time() + 600.0  # seconds
                 while benchp.poll() is None:
                     sleep(5)
-                    with (root / "running.txt").open("a+") as f:
+                    with (dir / "running.txt").open("a+") as f:
                         f.write(rm_ansi_escape(non_block_read(dmesgp.stdout)))
                     if time() > timeout:
                         print("Timeout..killing benchmark")
                         benchp.kill()
                         raise TimeoutError()
-              
-        
-
 
                 sleep(1)
 
-                with (root / "running.txt").open("a+") as f:
+                with (dir / "running.txt").open("a+") as f:
                     f.write(rm_ansi_escape(non_block_read(dmesgp.stdout)))
 
-                # with (dir / "running.txt").open("a+") as f:
-                #     f.write(rm_ansi_escape(non_block_read(qemu.stdout)))
-
                 print("save out")
-                os.system(f"sudo cat /proc/alloc/out | tee {dir}/out_{order}.csv")
-                # with (dir / f"out_{order}.csv").open("w+") as f:
-                #     out = os.system("sudo cat /proc/alloc/out", output=True)
-                #     f.write(out)
+                check_call(
+                    [f"sudo cat /proc/alloc/out | tee {dir}/out_{order}.csv"], shell=True)
 
         except Exception as e:
-            # with (dir / "error.txt").open("w+") as f:
-            #     f.write(rm_ansi_escape(non_block_read(qemu.stdout)))
-            # qemu.terminate()
+            with (dir / "error.txt").open("w+") as f:
+                f.write(rm_ansi_escape(non_block_read(dmesgp.stdout)))
+            dmesgp.send_signal(2)
             raise e
 
-
-        with (root / "running.txt").open("a+") as f:
+        with (dir / "running.txt").open("a+") as f:
             f.write(rm_ansi_escape(non_block_read(dmesgp.stdout)))
 
-
         print("terminate...")
-        # Kill dmesg logging
         dmesgp.send_signal(2)
-        # qemu.terminate()
         sleep(3)
 
 
