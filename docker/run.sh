e6cf1cd47197083daa33e6a81d2163c6734216f2
Lars Wrenger <wrenger@sra.uni-hannover.de>
2023-05-08 14:28:45 +0200

    :wrench: Update docker config

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,2 @@
-docker run -p 2222:22 --device=/dev/kvm --rm ghcr.io/luhsra/llfree_ae
+mkdir -p artifact
+docker run -p 2222:22 --device=/dev/kvm --rm ghcr.io/luhsra/llfree_ae -v "$(pwd)"/artifact:/home/user/llfree-bench/artifact
