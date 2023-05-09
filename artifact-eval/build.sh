cdbcd51a0e5430068acb42db55b711a28a22320e
Lars Wrenger <wrenger@sra.uni-hannover.de>
2023-05-09 15:12:40 +0200

    Minor changes

--- artifact-eval/build.sh
+++ artifact-eval/build.sh
@@ -3,7 +3,7 @@ cp -r ../build-alloc .
 cp -r ../build-buddy .
 cp -r ../build-llfree .
 cp ../resources/hda.qcow2 .
-docker build -t ghcr.io/luhsra/llfree_ae .
+docker build -t ghcr.io/luhsra/llfree_ae . $@
 rm -r ./build-alloc
 rm -r ./build-buddy
 rm -r ./build-llfree
