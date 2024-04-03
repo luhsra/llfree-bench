9e9848a659bea9b7f0471fc0b66dc09d7f2daa8c
Lars Wrenger <lars@wrenger.net>
2024-04-03 23:45:44 +0200

    :sparkles: Allocation tracing

--- max_power.sh
+++ max_power.sh
@@ -1,16 +1,24 @@
 set -e
 
-MIN_CPU=$(cat /sys/devices/system/cpu/intel_pstate/min_perf_pct)
+MIN_CPU=0
+if [ -f "/sys/devices/system/cpu/intel_pstate/min_perf_pct" ]; then
+    MIN_CPU=$(cat /sys/devices/system/cpu/intel_pstate/min_perf_pct)
+fi
+
 cleanup()
 {
     echo reenable powersaving
     echo powersave | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
-    echo $MIN_CPU >> /sys/devices/system/cpu/intel_pstate/min_perf_pct
+    if [ -f "/sys/devices/system/cpu/intel_pstate/min_perf_pct" ]; then
+        echo $MIN_CPU >> /sys/devices/system/cpu/intel_pstate/min_perf_pct
+    fi
 }
 trap cleanup EXIT HUP INT QUIT KILL SEGV TERM
 
 echo disable powersaving
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
-echo 100 >> /sys/devices/system/cpu/intel_pstate/min_perf_pct
+if [ -f "/sys/devices/system/cpu/intel_pstate/min_perf_pct" ]; then
+    echo 100 >> /sys/devices/system/cpu/intel_pstate/min_perf_pct
+fi
 
 sudo nice -n -20 sudo -u $SUDO_USER $@
